c71d4fc8c0033dfa9af707056e6e021010bd172b
loe <loe.bi@outlook.com>
2022-12-01 20:24:51 +0000

    A working loading program

--- emoji.py
+++ emoji.py
@@ -1,22 +1,46 @@
 import settings
 import os
+from PIL import Image
+import numpy as np
 
 class Emoji:
-    def __init__(self, data_folder: str, company_name: str=""):
+    def __init__(self, data_folder: str, company_name: str="", output_size: tuple=(0,0)):
         """
         Takes an company name as an input, load the data from .data folder. The company name is default to the data folder name.
         """
-        pass
+        self.dir: str = data_folder
+        self.name: str = company_name
+        self.pic_file_names = self._load()
+        self.output_size = output_size
+
+        self._index: int = 0
     
     # API
     
     
     # Subprocess
-    def _load(self):
-        pass
+    def _load(self) -> list:
+        all_content = os.listdir(self.dir)
+        valid_content = []
+        for i in all_content:
+            if i.endswith((".jpg", ".jpeg", ".png")) and \
+                os.path.isfile(os.path.join(self.dir,i)):
+                valid_content.append(i)
+        return valid_content
 
     def __next__(self):
-        pass
+        self._index += 1
+        if self._index > len(self.pic_file_names):
+            raise StopIteration
+
+        # Read image
+        im = Image.open(os.path.join(self.dir, self.pic_file_names[self._index - 1]))
+        if self.output_size == (0,0):
+            pixels = np.array(im.getdata())
+            return pixels
+        else:
+            pass
+        return 
 
     def __iter__(self):
         return self
